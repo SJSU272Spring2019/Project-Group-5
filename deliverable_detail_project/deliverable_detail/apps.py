d63726863836c16eb8419f722bbbe9257e7782be
bradleyfoy <bradley.foy@sjsu.edu>
2019-04-21 09:13:08 -0700

    Delete
    Delete all

--- deliverable_detail_project/deliverable_detail/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DeliverableDetailConfig(AppConfig):
-    name = 'deliverable_detail'
--- deliverable_detail_project/queue_page/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class QueuePageConfig(AppConfig):
-    name = 'queue_page'
