a635617028b401f5297f65c5ec8ec519e85bac08
bradleyfoy <bradley.foy@sjsu.edu>
2019-04-25 21:50:39 -0700

    Thursday 04252019

--- training_management/deliverable_detail/views.py
+++ training_management/deliverable_detail/views.py
@@ -20,6 +20,7 @@ def deliverable_detail_submit(request):
             my_deployment_detail_record.course_name = request.POST['course_name']
             my_deployment_detail_record.course_description = request.POST['course_description']
             my_deployment_detail_record.category = request.POST['category']
+            my_deployment_detail_record.project_code = request.POST['project_code']
         else:
             my_deployment_detail_record = temp_deployment_record[0]
 
@@ -47,11 +48,6 @@ def save_detail(request):
     else:
         temp_record_date = datetime.strptime(request.POST['record_date'], '%m/%d/%Y')
 
-    if request.POST['processing_complete_date'] == '':
-        temp_processing_complete_date = None
-    else:
-        temp_processing_complete_date = datetime.strptime(request.POST['processing_complete_date'], '%m/%d/%Y')
-
     if request.POST['post_processing_complete_date'] == '':
         temp_post_processing_complete_date = None
     else:
@@ -74,7 +70,6 @@ def save_detail(request):
                                                    notes=request.POST['notes'],
                                                    content_complete_date=temp_content_complete_date,
                                                    record_date=temp_record_date,
-                                                   processing_complete_date=temp_processing_complete_date,
                                                    post_processing_complete_date=temp_post_processing_complete_date,
                                                    target_deploy_date=temp_target_deploy_date)
         my_deliverable_detail_record.save()
@@ -94,7 +89,6 @@ def save_detail(request):
         my_deliverable_detail_record.notes = request.POST['notes']
         my_deliverable_detail_record.content_complete_date = temp_content_complete_date
         my_deliverable_detail_record.record_date = temp_record_date
-        my_deliverable_detail_record.processing_complete_date = temp_processing_complete_date
         my_deliverable_detail_record.post_processing_complete_date = temp_post_processing_complete_date
         my_deliverable_detail_record.target_deploy_date = temp_target_deploy_date
         my_deliverable_detail_record.save()
