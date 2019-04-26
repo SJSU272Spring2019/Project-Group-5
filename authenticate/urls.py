a9828525d69495e333b4438e1602899938d3a3d7
bradleyfoy <bradley.foy@sjsu.edu>
2019-04-25 22:04:22 -0700

    New start

---
+++ authenticate/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.my_login),
+    path('login/', views.my_login),
+    path('create_account/', views.create_account),
+    path('current_user/', views.get_profile_data)
+]
